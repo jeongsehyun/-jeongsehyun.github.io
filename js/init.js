8aa86a1af809cfedefef06a010911c78c29276be
jsh <sehyun.dev@gmail.com>
2023-09-27 00:58:55 +0900

    test

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 window.onload = function () {
   fetch("./posts/230926.md")
     .then((response) => response.text())
-    .then((result) => (document.getElementById("content").innerHTML = marked(result)));
+    .then((result) => (document.getElementById("content").innerHTML = marked.marked(result)));
 };
